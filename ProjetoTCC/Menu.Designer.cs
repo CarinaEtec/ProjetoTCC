c616eded41e463b44843757dc5df996e8db33059
CarinaEtec <miguel.sgarbozza@hotmail.com>
2019-03-17 00:02:20 -0300

    Quarta atualização. Foco nas imagens

---
+++ ProjetoTCC/Menu.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ProjetoTCC
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.btnAptos = new System.Windows.Forms.Button();
+            this.btnAnimais = new System.Windows.Forms.Button();
+            this.btnCriancas = new System.Windows.Forms.Button();
+            this.btnOcorrencias = new System.Windows.Forms.Button();
+            this.btnLocacao = new System.Windows.Forms.Button();
+            this.btnObras = new System.Windows.Forms.Button();
+            this.btnVeiculos = new System.Windows.Forms.Button();
+            this.btnMoradores = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnAdicionarDados = new System.Windows.Forms.Button();
+            this.btnAlterarDados = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAptos
+            // 
+            this.btnAptos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAptos.Image = ((System.Drawing.Image)(resources.GetObject("btnAptos.Image")));
+            this.btnAptos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAptos.Location = new System.Drawing.Point(80, 47);
+            this.btnAptos.Name = "btnAptos";
+            this.btnAptos.Size = new System.Drawing.Size(220, 75);
+            this.btnAptos.TabIndex = 1;
+            this.btnAptos.Text = "Aptos";
+            this.btnAptos.UseVisualStyleBackColor = true;
+            this.btnAptos.Click += new System.EventHandler(this.btnAptos_Click);
+            // 
+            // btnAnimais
+            // 
+            this.btnAnimais.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnimais.Image = ((System.Drawing.Image)(resources.GetObject("btnAnimais.Image")));
+            this.btnAnimais.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAnimais.Location = new System.Drawing.Point(395, 147);
+            this.btnAnimais.Name = "btnAnimais";
+            this.btnAnimais.Size = new System.Drawing.Size(220, 75);
+            this.btnAnimais.TabIndex = 3;
+            this.btnAnimais.Text = "Animais";
+            this.btnAnimais.UseVisualStyleBackColor = true;
+            this.btnAnimais.Click += new System.EventHandler(this.btnAnimais_Click);
+            // 
+            // btnCriancas
+            // 
+            this.btnCriancas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriancas.Image = ((System.Drawing.Image)(resources.GetObject("btnCriancas.Image")));
+            this.btnCriancas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCriancas.Location = new System.Drawing.Point(80, 147);
+            this.btnCriancas.Name = "btnCriancas";
+            this.btnCriancas.Size = new System.Drawing.Size(220, 75);
+            this.btnCriancas.TabIndex = 2;
+            this.btnCriancas.Text = "Crianças";
+            this.btnCriancas.UseVisualStyleBackColor = true;
+            this.btnCriancas.Click += new System.EventHandler(this.btnCriancas_Click);
+            // 
+            // btnOcorrencias
+            // 
+            this.btnOcorrencias.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOcorrencias.Image = ((System.Drawing.Image)(resources.GetObject("btnOcorrencias.Image")));
+            this.btnOcorrencias.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOcorrencias.Location = new System.Drawing.Point(395, 267);
+            this.btnOcorrencias.Name = "btnOcorrencias";
+            this.btnOcorrencias.Size = new System.Drawing.Size(220, 75);
+            this.btnOcorrencias.TabIndex = 5;
+            this.btnOcorrencias.Text = "Ocorrências";
+            this.btnOcorrencias.UseVisualStyleBackColor = true;
+            this.btnOcorrencias.Click += new System.EventHandler(this.btnOcorrencias_Click);
+            // 
+            // btnLocacao
+            // 
+            this.btnLocacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocacao.Image = ((System.Drawing.Image)(resources.GetObject("btnLocacao.Image")));
+            this.btnLocacao.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLocacao.Location = new System.Drawing.Point(80, 267);
+            this.btnLocacao.Name = "btnLocacao";
+            this.btnLocacao.Size = new System.Drawing.Size(220, 75);
+            this.btnLocacao.TabIndex = 4;
+            this.btnLocacao.Text = "Locação";
+            this.btnLocacao.UseVisualStyleBackColor = true;
+            this.btnLocacao.Click += new System.EventHandler(this.btnLocacao_Click);
+            // 
+            // btnObras
+            // 
+            this.btnObras.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnObras.Image = ((System.Drawing.Image)(resources.GetObject("btnObras.Image")));
+            this.btnObras.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnObras.Location = new System.Drawing.Point(395, 391);
+            this.btnObras.Name = "btnObras";
+            this.btnObras.Size = new System.Drawing.Size(220, 75);
+            this.btnObras.TabIndex = 7;
+            this.btnObras.Text = "Obras";
+            this.btnObras.UseVisualStyleBackColor = true;
+            this.btnObras.Click += new System.EventHandler(this.btnObras_Click);
+            // 
+            // btnVeiculos
+            // 
+            this.btnVeiculos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVeiculos.Image = ((System.Drawing.Image)(resources.GetObject("btnVeiculos.Image")));
+            this.btnVeiculos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVeiculos.Location = new System.Drawing.Point(80, 391);
+            this.btnVeiculos.Name = "btnVeiculos";
+            this.btnVeiculos.Size = new System.Drawing.Size(220, 75);
+            this.btnVeiculos.TabIndex = 6;
+            this.btnVeiculos.Text = "Veículos";
+            this.btnVeiculos.UseVisualStyleBackColor = true;
+            this.btnVeiculos.Click += new System.EventHandler(this.btnVeiculos_Click);
+            // 
+            // btnMoradores
+            // 
+            this.btnMoradores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoradores.Image = ((System.Drawing.Image)(resources.GetObject("btnMoradores.Image")));
+            this.btnMoradores.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMoradores.Location = new System.Drawing.Point(395, 47);
+            this.btnMoradores.Name = "btnMoradores";
+            this.btnMoradores.Size = new System.Drawing.Size(220, 75);
+            this.btnMoradores.TabIndex = 8;
+            this.btnMoradores.Text = "Moradores";
+            this.btnMoradores.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(275, 627);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(114, 52);
+            this.btnSair.TabIndex = 9;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnAdicionarDados
+            // 
+            this.btnAdicionarDados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionarDados.Location = new System.Drawing.Point(80, 516);
+            this.btnAdicionarDados.Name = "btnAdicionarDados";
+            this.btnAdicionarDados.Size = new System.Drawing.Size(220, 75);
+            this.btnAdicionarDados.TabIndex = 10;
+            this.btnAdicionarDados.Text = "Adicionar Dados";
+            this.btnAdicionarDados.UseVisualStyleBackColor = true;
+            this.btnAdicionarDados.Click += new System.EventHandler(this.btnAdicionarDados_Click);
+            // 
+            // btnAlterarDados
+            // 
+            this.btnAlterarDados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlterarDados.Location = new System.Drawing.Point(395, 516);
+            this.btnAlterarDados.Name = "btnAlterarDados";
+            this.btnAlterarDados.Size = new System.Drawing.Size(220, 75);
+            this.btnAlterarDados.TabIndex = 11;
+            this.btnAlterarDados.Text = "Alterar Dados";
+            this.btnAlterarDados.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(684, 691);
+            this.Controls.Add(this.btnAlterarDados);
+            this.Controls.Add(this.btnAdicionarDados);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnMoradores);
+            this.Controls.Add(this.btnObras);
+            this.Controls.Add(this.btnVeiculos);
+            this.Controls.Add(this.btnOcorrencias);
+            this.Controls.Add(this.btnLocacao);
+            this.Controls.Add(this.btnAnimais);
+            this.Controls.Add(this.btnCriancas);
+            this.Controls.Add(this.btnAptos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAptos;
+        private System.Windows.Forms.Button btnAnimais;
+        private System.Windows.Forms.Button btnCriancas;
+        private System.Windows.Forms.Button btnOcorrencias;
+        private System.Windows.Forms.Button btnLocacao;
+        private System.Windows.Forms.Button btnObras;
+        private System.Windows.Forms.Button btnVeiculos;
+        private System.Windows.Forms.Button btnMoradores;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnAdicionarDados;
+        private System.Windows.Forms.Button btnAlterarDados;
+    }
+}
---
+++ ProjetoTCC/Veiculos.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace ProjetoTCC
+{
+    partial class Veiculos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Veiculos));
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Apto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bloco = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBusca = new System.Windows.Forms.TextBox();
+            this.rbtProprietario = new System.Windows.Forms.RadioButton();
+            this.rbtApto = new System.Windows.Forms.RadioButton();
+            this.rbtBloco = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbtPlaca = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(280, 609);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(113, 50);
+            this.btnVoltar.TabIndex = 22;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Apto,
+            this.Bloco,
+            this.Column2,
+            this.Column4,
+            this.Column1,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(22, 270);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(644, 339);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // Apto
+            // 
+            this.Apto.HeaderText = "Apto";
+            this.Apto.Name = "Apto";
+            this.Apto.ReadOnly = true;
+            // 
+            // Bloco
+            // 
+            this.Bloco.HeaderText = "Bloco";
+            this.Bloco.Name = "Bloco";
+            this.Bloco.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Proprietário";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Placa";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Marca";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cor";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(497, 134);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 34);
+            this.btnBuscar.TabIndex = 20;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtBusca
+            // 
+            this.txtBusca.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBusca.Location = new System.Drawing.Point(497, 86);
+            this.txtBusca.Name = "txtBusca";
+            this.txtBusca.Size = new System.Drawing.Size(100, 24);
+            this.txtBusca.TabIndex = 19;
+            // 
+            // rbtProprietario
+            // 
+            this.rbtProprietario.AutoSize = true;
+            this.rbtProprietario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtProprietario.Location = new System.Drawing.Point(353, 146);
+            this.rbtProprietario.Name = "rbtProprietario";
+            this.rbtProprietario.Size = new System.Drawing.Size(103, 22);
+            this.rbtProprietario.TabIndex = 18;
+            this.rbtProprietario.TabStop = true;
+            this.rbtProprietario.Text = "Proprietário";
+            this.rbtProprietario.UseVisualStyleBackColor = true;
+            // 
+            // rbtApto
+            // 
+            this.rbtApto.AutoSize = true;
+            this.rbtApto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtApto.Location = new System.Drawing.Point(353, 116);
+            this.rbtApto.Name = "rbtApto";
+            this.rbtApto.Size = new System.Drawing.Size(56, 22);
+            this.rbtApto.TabIndex = 17;
+            this.rbtApto.TabStop = true;
+            this.rbtApto.Text = "Apto";
+            this.rbtApto.UseVisualStyleBackColor = true;
+            // 
+            // rbtBloco
+            // 
+            this.rbtBloco.AutoSize = true;
+            this.rbtBloco.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtBloco.Location = new System.Drawing.Point(353, 88);
+            this.rbtBloco.Name = "rbtBloco";
+            this.rbtBloco.Size = new System.Drawing.Size(65, 22);
+            this.rbtBloco.TabIndex = 16;
+            this.rbtBloco.TabStop = true;
+            this.rbtBloco.Text = "Bloco";
+            this.rbtBloco.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(410, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 29);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "VEÍCULOS";
+            // 
+            // rbtPlaca
+            // 
+            this.rbtPlaca.AutoSize = true;
+            this.rbtPlaca.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtPlaca.Location = new System.Drawing.Point(353, 174);
+            this.rbtPlaca.Name = "rbtPlaca";
+            this.rbtPlaca.Size = new System.Drawing.Size(63, 22);
+            this.rbtPlaca.TabIndex = 23;
+            this.rbtPlaca.TabStop = true;
+            this.rbtPlaca.Text = "Placa";
+            this.rbtPlaca.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Veiculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(684, 691);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rbtPlaca);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBusca);
+            this.Controls.Add(this.rbtProprietario);
+            this.Controls.Add(this.rbtApto);
+            this.Controls.Add(this.rbtBloco);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Veiculos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Veiculos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bloco;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBusca;
+        private System.Windows.Forms.RadioButton rbtProprietario;
+        private System.Windows.Forms.RadioButton rbtApto;
+        private System.Windows.Forms.RadioButton rbtBloco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtPlaca;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
