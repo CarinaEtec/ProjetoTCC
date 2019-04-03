4666598303a9b618f8a15f0f39ac98ba9912ddfd
CarinaEtec <miguel.sgarbozza@hotmail.com>
2019-04-03 00:31:23 -0300

    Mudanças no Form e criação de telas Alteração

---
+++ ProjetoTCC/frmAltDados.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoTCC
+{
+    public partial class frmAltDados : Form
+    {
+        public frmAltDados()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCriancas_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmAltCriancas pirralhos = new frmAltCriancas();
+            pirralhos.Closed += (s, args) => this.Close();
+            pirralhos.Show();
+        }
+
+        private void btnAnimais_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmAltPets pets = new frmAltPets();
+            pets.Closed += (s, args) => this.Close();
+            pets.Show();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmMenu menuzinho = new frmMenu();
+            menuzinho.Closed += (s, args) => this.Close();
+            menuzinho.Show();
+        }
+    }
+}
---
+++ ProjetoTCC/frmCriancas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoTCC
+{
+    public partial class frmCriancas : Form
+    {
+        public frmCriancas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmMenu menuzinho = new frmMenu();
+            menuzinho.Closed += (s, args) => this.Close();
+            menuzinho.Show();
+        }
+    }
+}
